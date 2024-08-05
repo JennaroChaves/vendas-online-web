f09ec1d74c3324fdf859a29e1c1b76ab80190a79
Jennaro_FrontEnd <jennaro.chaves@mamboo.co.ao>
2024-08-05 12:07:46 +0100

    feat: create module login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,10 +1,27 @@
-import { BackgroundImage } from "../styles/loginScreen.styles";
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreen,
+  LimitedContainer,
+  LogoImage,
+} from "../styles/loginScreen.styles";
 import backgroundimg from "../../../../public/background.png";
+import logo from "../../../../public/logo.png";
+import Input from "../../../shared/inputs/input/input";
 
 const LoginScreen = () => {
   return (
     <div>
-      <BackgroundImage src={backgroundimg} />
+      <ContainerLoginScreen> 
+        <ContainerLogin>
+          <LimitedContainer>
+            <LogoImage src={logo} />
+            <Input title="USUÁRIO"/>
+            <Input title="SENHA"/>
+          </LimitedContainer>
+        </ContainerLogin>
+        <BackgroundImage src={backgroundimg} />
+      </ContainerLoginScreen>
     </div>
   );
 };
